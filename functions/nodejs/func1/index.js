4bb96c31e8742bee77493610d120df2250711215
zhangying.9707 <zhangying.9707@bytedance.com>
2023-10-12 16:00:40 +0800

    17

--- functions/nodejs/func1/index.js
+++ functions/nodejs/func1/index.js
@@ -30,6 +30,7 @@ module.exports = async function (params, context, logger) {
   logger.info("commit部署14")
   logger.info("commit部署15")
   logger.info("commit部署16")
+  logger.info("commit部署17")
 
 
 }
